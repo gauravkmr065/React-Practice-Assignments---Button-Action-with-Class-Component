f21d63bd9d0c55dddb601ec302f81b09165b3536
gaurav saxena <42857179+gauravkmr065@users.noreply.github.com>
2021-05-09 12:51:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,13 @@ class App extends Component {
     constructor(props) {
 		super(props);
 	     this.state = {str: ''}
-	    this.showPara=this.showPara.bind(this);
 	};
-    showPara(){
-		 console.log("click");
-        let paradata = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-	   this.setState({str:paradata});
-    }
-
+   
     render() {
     	return(
     		<div id="main">
-		     <p id="para">{this.state.str}</p>
-            <button id="click" onClick={this.showPara}>Click Me</button>
+		{this.state.str=='b' &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}    
+            <button id="click" onClick={()=>this.setState({str:'b'})}>Click Me</button>
     		</div>
     	);
     }
